c01cf7e98630e4c5bf28bee269a1e840b7aa634e
toreseb <toreseb@gmail.com>
2022-01-05 12:34:53 +0100

    Timer working

--- 3UgersProjekt/src/framework/main.java
+++ 3UgersProjekt/src/framework/main.java
@@ -1,6 +1,11 @@
 package framework;
 
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import gameObjects.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +13,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	public static ArrayList<GameObject> objList = new ArrayList<GameObject>();
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -15,9 +23,22 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
+    	System.out.println("Hi");
+    	Timer t = new Timer();
+    	t.scheduleAtFixedRate(new TimerTask(){
+		    @Override
+		    public void run(){
+		       Main.run();
+		    }
+		},0,16);
         launch(args);
+        t.cancel();
+        System.out.println("Hi3");
+    }
+    
+    static void run() {
+    	System.out.println("Hi2");
     }
 }
---
+++ 3UgersProjekt/src/gameObjects/GameObject.java
@@ -0,0 +1,11 @@
+package gameObjects;
+
+import framework.Main;
+
+public class GameObject {
+	
+	public GameObject() {
+		Main.objList.add(this);
+	}
+
+}
