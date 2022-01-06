53b51a11eb141a2e92f58eb13ff82447d3293412
WilliTheDude <holbergw@gmail.com>
2022-01-06 10:13:38 +0100

    updated gameObject

--- 3UgersProjekt/src/gameObjects/GameObject.java
+++ 3UgersProjekt/src/gameObjects/GameObject.java
@@ -1,15 +1,54 @@
 package gameObjects;
 
 import framework.Main;
+import java.util.Vector;
+
+/**
+ * @author williamholberg
+ *
+ */
+
 
 public abstract class GameObject {
 	
-	public GameObject() {
-		Main.objList.add(this);
+
+	private Vector<Integer> vectorPos; 		// The position for the palyers
+	private int width, height; 				// the width and hight of the shaps
+	
+	
+	// Constructor
+	public GameObject(int posX, int posY, int width, int height) {
+		vectorPos = new Vector<Integer>(posX, posY);
+		this.width = width;
+		this.height = height;
+		
+		Main.objList.add(this);// Adds the playr to a list.
 	}
 	
-	public void run() {
+	
+	public abstract void shape();
+	
+	public abstract void run();
+	
+	
+	public void updatePosition() {
 		
 	}
-
+	
+	public void collision() {
+		
+	}
+	
+	// Getters
+	public Vector<Integer> getVectorPos() {
+		return vectorPos;
+	}
+	
+	
+	// to string method
+	public String toString() {
+		return "Players position: " ;
+	}
+	
+ 
 }
