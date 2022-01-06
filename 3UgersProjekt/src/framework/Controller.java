b7a75f81907a3a104cab43094195507170db414f
WilliTheDude <holbergw@gmail.com>
2022-01-06 11:55:35 +0100

    Updated the controller

--- 3UgersProjekt/src/framework/Controller.java
+++ 3UgersProjekt/src/framework/Controller.java
@@ -1,7 +1,16 @@
 package framework;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
 	
+	@FXML
+	TextField heightTextField, widthTextField;
+	@FXML
+	Button submitButton;
+	
+	int height, width;
 	
 	public void changeScene() {
 		
