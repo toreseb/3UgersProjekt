8faa74de0feefb429ffeaaeb6ac2261b7e75219b
Student214933 <s214933@student.dtu.dk>
2022-01-13 12:16:33 +0100

    Fikset PowerUp billede problem
    Tak Tore :)

--- 3UgersProjekt/src/gameObjects/PowerUp.java
+++ 3UgersProjekt/src/gameObjects/PowerUp.java
@@ -24,16 +24,12 @@ public abstract class PowerUp extends GameObject {
 	private int counter = 0;
 	private int nr;
 
-	public PowerUp(double posX, double posY, int width, int height,int nr) {
+	public PowerUp(double posX, double posY, int width, int height) {
 		super(posX, posY, width, height);
-		this.nr = nr;
 	}
 
 	void initShape() {
 		// TODO Auto-generated method stub
-		if (nr == 1) {
-			image = new Image("2.png");
-		} // Tilføj en for hvert power up
 
 		imageView = new ImageView(image);
 
